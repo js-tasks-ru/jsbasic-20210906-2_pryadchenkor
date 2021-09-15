1cd6ccdec70e1ae179dfe15738a23ca8da6a14fc
keanu <keanu@inernetbs.net>
2021-09-15 12:03:03 +0300

    made calculator and change condition in task 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,10 +1,3 @@
 function isEmpty(obj) {
-  // for (let data in obj) {
-  //   if (obj.hasOwnProperty(data)) {
-  //     return false;
-  //   }
-  // }
-  // return true;
-  return Object.keys(obj).length === 0
-    && obj.constructor === Object;
+  return Object.keys(obj).length === 0;
 }
