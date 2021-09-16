a0e6d82cba900f56789d3ace2673b14257422c04
keanu <keanu@inernetbs.net>
2021-09-16 18:24:05 +0300

    made MinMax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  let Numbers = str.split(" ").filter((item) => !isNaN(Number(item)));
+  return {
+    min: Math.min.apply(null, Numbers),
+    max: Math.max.apply(null, Numbers),
+  };
 }
+console.log(getMinMax("1 и -5.8 или 10 хотя 34 + -5.3 и 73"));
