61e2d4cbb0cf9e3d3969d76443ac67175b940625
keanu <keanu@inernetbs.net>
2021-09-22 18:39:12 +0300

    made function DiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,16 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const tableRow = tabdiagonalRedle.getElementsByTagName("tr");
+
+  const trArr = [...tableRow];
+  const newArr = [];
+  for (let item of trArr) {
+    const tableData = item.getElementsByTagName("td");
+    const tdArr = [...tableData];
+    newArr.push(tdArr);
+  }
+
+  console.log(newArr);
+  for (let i = 0; i < newArr.length; i++) {
+    newArr[i][i].style.backgroundColor = "red";
+  }
 }
